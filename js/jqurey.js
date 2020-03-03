9e59f27f4581a24aa949055c733823140c699b95
FilipThureson <60510154+FilipThureson@users.noreply.github.com>
2020-03-03 19:34:48 +0100

    All sites one file

--- js/jqurey.js
+++ js/jqurey.js
@@ -8,4 +8,68 @@ $(document).ready(function(){
    $('.header').toggleClass('headeractive')
    $('.fadeout').toggleClass('fadeoutactive')
   })
+
+  $('#start').click(function() {
+    $('.chassi').css("z-index", "1");
+    $('.cpu').css("z-index", "1");
+    $('.gpu').css("z-index", "1");
+    $('.mobo').css("z-index", "1");
+    $('.ram').css("z-index", "1");
+    $('.utrymme').css("z-index", "1");
+    $('.main').css("z-index", "2");
+  })
+  $('#chassi').click(function() {
+    $('.chassi').css("z-index", "2");
+    $('.cpu').css("z-index", "1");
+    $('.gpu').css("z-index", "1");
+    $('.mobo').css("z-index", "1");
+    $('.ram').css("z-index", "1");
+    $('.utrymme').css("z-index", "1");
+    $('.main').css("z-index", "1");
+  })
+  $('#mobo').click(function() {
+    $('.chassi').css("z-index", "1");
+    $('.cpu').css("z-index", "1");
+    $('.gpu').css("z-index", "1");
+    $('.mobo').css("z-index", "2");
+    $('.ram').css("z-index", "1");
+    $('.utrymme').css("z-index", "1");
+    $('.main').css("z-index", "1");
+  })
+  $('#cpu').click(function() {
+    $('.chassi').css("z-index", "1");
+    $('.cpu').css("z-index", "2");
+    $('.gpu').css("z-index", "1");
+    $('.mobo').css("z-index", "1");
+    $('.ram').css("z-index", "1");
+    $('.utrymme').css("z-index", "1");
+    $('.main').css("z-index", "1");
+  })
+  $('#gpu').click(function() {
+    $('.chassi').css("z-index", "1");
+    $('.cpu').css("z-index", "1");
+    $('.gpu').css("z-index", "2");
+    $('.mobo').css("z-index", "1");
+    $('.ram').css("z-index", "1");
+    $('.utrymme').css("z-index", "1");
+    $('.main').css("z-index", "1");
+  })
+  $('#ram').click(function() {
+    $('.chassi').css("z-index", "1");
+    $('.cpu').css("z-index", "1");
+    $('.gpu').css("z-index", "1");
+    $('.mobo').css("z-index", "1");
+    $('.ram').css("z-index", "2");
+    $('.utrymme').css("z-index", "1");
+    $('.main').css("z-index", "1");
+  })
+  $('#hdd').click(function() {
+    $('.chassi').css("z-index", "1");
+    $('.cpu').css("z-index", "1");
+    $('.gpu').css("z-index", "1");
+    $('.mobo').css("z-index", "1");
+    $('.ram').css("z-index", "1");
+    $('.utrymme').css("z-index", "2");
+    $('.main').css("z-index", "1");
+  })
 })
