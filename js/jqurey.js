53309d230cf9d5194dd927a6dc8fce9d5d9f33af
FilipThureson <60510154+FilipThureson@users.noreply.github.com>
2020-03-04 08:34:29 +0100

    text removed

--- js/jqurey.js
+++ js/jqurey.js
@@ -1,6 +1,15 @@
 
 $(document).ready(function(){
 
+  $('.btn1').click(function(){
+    $('.sidebar').toggleClass('active');
+    $('.sidebarbtn').toggleClass('toggle');
+   $('.main').toggleClass('mainactive')
+   $('.footer').toggleClass('footeractive')
+   $('.header').toggleClass('headeractive')
+   $('.fadeout').toggleClass('fadeoutactive')
+  })
+
   $('.sidebarbtn').click(function(){
     $('.sidebar').toggleClass('active');
     $('.sidebarbtn').toggleClass('toggle');
@@ -26,6 +35,7 @@ $(document).ready(function(){
     $('.ram').css("z-index", "1");
     $('.utrymme').css("z-index", "1");
     $('.main').css("z-index", "2");
+    $('.video').css("z-index", "1")
   })
   $('#chassi').click(function() {
     $('.chassi').css("z-index", "2");
@@ -35,6 +45,8 @@ $(document).ready(function(){
     $('.ram').css("z-index", "1");
     $('.utrymme').css("z-index", "1");
     $('.main').css("z-index", "1");
+    $('.video').css("z-index", "1");
+
   })
   $('#mobo').click(function() {
     $('.chassi').css("z-index", "1");
@@ -44,6 +56,7 @@ $(document).ready(function(){
     $('.ram').css("z-index", "1");
     $('.utrymme').css("z-index", "1");
     $('.main').css("z-index", "1");
+    $('.video').css("z-index", "1");
   })
   $('#cpu').click(function() {
     $('.chassi').css("z-index", "1");
@@ -53,6 +66,7 @@ $(document).ready(function(){
     $('.ram').css("z-index", "1");
     $('.utrymme').css("z-index", "1");
     $('.main').css("z-index", "1");
+    $('.video').css("z-index", "1");
   })
   $('#gpu').click(function() {
     $('.chassi').css("z-index", "1");
@@ -62,6 +76,7 @@ $(document).ready(function(){
     $('.ram').css("z-index", "1");
     $('.utrymme').css("z-index", "1");
     $('.main').css("z-index", "1");
+    $('.video').css("z-index", "1");
   })
   $('#ram').click(function() {
     $('.chassi').css("z-index", "1");
@@ -71,6 +86,7 @@ $(document).ready(function(){
     $('.ram').css("z-index", "2");
     $('.utrymme').css("z-index", "1");
     $('.main').css("z-index", "1");
+    $('.video').css("z-index", "1");
   })
   $('#hdd').click(function() {
     $('.chassi').css("z-index", "1");
@@ -80,5 +96,17 @@ $(document).ready(function(){
     $('.ram').css("z-index", "1");
     $('.utrymme').css("z-index", "2");
     $('.main').css("z-index", "1");
+    $('.video').css("z-index", "1");
   })
+  $('#video').click(function() {
+     $('.chassi').css("z-index", "1");
+     $('.cpu').css("z-index", "1");
+     $('.gpu').css("z-index", "1");
+     $('.mobo').css("z-index", "1");
+     $('.ram').css("z-index", "1");
+     $('.utrymme').css("z-index", "1");
+     $('.main').css("z-index", "1");
+     $('.video').css("z-index", "2");
+   });
+
 })
